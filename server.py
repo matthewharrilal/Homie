ffe8f43c7d92c769fd07d944cf0d4e49c97ebd4b
Matthew Harrilal <matthewharrilal@gmail.com>
2018-01-27 15:55:32 -0800

    Implemented the basic user functions have to implement the fetching of the wallet balance for the user

--- server.py
+++ server.py
@@ -14,11 +14,11 @@ from bson.json_util import dumps
 
 app = Flask(__name__)
 client = MongoClient('mongodb://localhost:27017/')
-app.db = client.orchard
+app.db = client.homie
 database = app.db
 rounds = app.bcrypt_rounds = 5
 api = Api(app)
-orchard_collection = database.orchard_collection
+homie_collection = database.homie_collection
 
 
 def authenticated_request(func):
@@ -31,7 +31,7 @@ def authenticated_request(func):
         email,password = decode(auth_code)
 
         if email is not None and password is not None:
-            user_collection = database.orchard_collection
+            user_collection = database.homie_collection
             user = user_collection.find_one({'email': email})
             if user is not None:
                 encoded_password = password.encode('utf-8')
@@ -53,7 +53,7 @@ class User(Resource):
         requested_json = request.json
 
         # So we specfically have to get the password so we can essentially hash it in our server
-        requested_password = request.json.get('password')
+        requested_password = requested_json.get('password')
 
         # Once we have the users password we can now be able to encode the users password in plain text and then it can be hashed
         encoded_password = requested_password.encode('utf-8')
@@ -61,7 +61,7 @@ class User(Resource):
         requested_json['password'] = hashed
 
         if 'email' in requested_json and 'password' in requested_json:
-            orchard_collection.insert_one(requested_json)
+            homie_collection.insert_one(requested_json)
             requested_json.pop('password')
             print('The user has succesfully been implemented to the database')
             return(requested_json, 201, None)
@@ -80,7 +80,7 @@ class User(Resource):
 
         # Now we need to find the user in the database
         # pdb.set_trace()
-        user_find = orchard_collection.find_one({'email': auth.username})
+        user_find = homie_collection.find_one({'email': auth.username})
 
         # Now we esentially implement the error handling
         if user_find is not None:
@@ -97,12 +97,12 @@ class User(Resource):
         # First we find the users
         auth = request.authorization
 
-        user_query = orchard_collection.find_one({'email': auth.username})
+        user_query = homie_collection.find_one({'email': auth.username})
 
         # Now we do some simple error handling to see if the user exist
 
         if user_query is not None:
-            orchard_collection.remove(user_query)
+            homie_collection.remove(user_query)
             user_query.pop('password')
             print("The user has been removed")
             return(user_query, 204, None)
@@ -110,7 +110,7 @@ class User(Resource):
             print("The user could not be deleted")
             return(None, 404, None)
 
-api.add_resource(User, '/orchard_users')
+api.add_resource(User, '/users')
 
 @api.representation('application/json')
 def output_json(data, code, headers=None):

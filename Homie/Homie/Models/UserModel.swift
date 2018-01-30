3056c791f2724841e8aef5b8efd2e32bebe2752f
Matthew Harrilal <matthewharrilal@gmail.com>
2018-01-30 14:53:15 -0800

    Fixed the user modeling to get the user json

--- Homie/Homie/Models/UserModel.swift
+++ Homie/Homie/Models/UserModel.swift
@@ -13,21 +13,29 @@ class User: Codable {
     var email: String?
     var password: String?
     var credential: String?
-    init(email: String?, password: String?) {
+    var bio: String?
+    var profilePicture: String?
+    init(email: String?, password: String?, bio: String?=nil, profilePicture: String?=nil) {
         self.email = email
         self.password = password
+        self.bio = bio
+        self.profilePicture = profilePicture
         self.credential = BasicAuth.generateBasicAuthHeader(email: email!, password: password!)
     }
     
     enum TopLevelKeys: String, CodingKey {
         case email
         case password
+        case profilePicture = "profile_picture"
+        case bio
     }
     
     required convenience init(from decoder: Decoder) {
         let container = try? decoder.container(keyedBy: TopLevelKeys.self)
         let email = try? container?.decodeIfPresent(String.self, forKey: .email)
         let password = try? container?.decodeIfPresent(String.self, forKey: .password)
-        self.init(email: email!, password: password!)
+        let profilePicture = try? container?.decodeIfPresent(String.self, forKey: .profilePicture)
+        let bio = try? container?.decodeIfPresent(String.self, forKey: .bio)
+        self.init(email: email!, password: password!, bio: bio!, profilePicture: profilePicture!)
     }
 }

68d1b213a1c8e01bbd503bebf17862df3e2dc592
Matthew Harrilal <matthewharrilal@gmail.com>
2018-01-30 15:42:38 -0800

    Implemented the profile picture feature as well as setting the bio

--- Homie/Homie/View Controllers/ProfileViewController.swift
+++ Homie/Homie/View Controllers/ProfileViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 import Kingfisher
 
 class ProfileViewController: UIViewController {
-   
+    var user: User?
+
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var userBioTextView: UITextView!
     @IBOutlet weak var usernameLabel: UILabel!
@@ -20,17 +21,14 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
         setUsername()
         
+
         
-//        print(user)
-//        print(user?.profilePicture)
-//        let url = URL(string: "http://eswob.org/wp-content/uploads/2016/02/a4517f74-ff7f-4b47-871c-6999c9a7395f-e1455305420868.png")
-//        print(url)
-//        profileImageView.kf.setImage(with: url)
-//        userBioTextView.text = user?.bio
-        
-        profileNetworking(target: .fetchProfiles, success: { (response) in
-            let json = try? response.mapJSON()
-            print(json)
+        userNetworking(target: .fetchUsers, success: { (response) in
+            guard let profilePicture = try? response.mapString(atKeyPath: "profile_picture") else {return}
+            guard let bio = try? response.mapString(atKeyPath: "bio") else {return}
+            let url = URL(string: profilePicture)
+            self.profileImageView.kf.setImage(with: url)
+            self.userBioTextView.text = bio
         }, error: { (error) in
             print(error)
         }, failure: { (MoyaError) in

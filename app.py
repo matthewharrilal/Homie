2a840973922245998ed6317ece12544398b8a0d4
Matthew Harrilal <matthewharrilal@gmail.com>
2018-01-30 12:27:08 -0800

    Made some changes

--- app.py
+++ app.py
@@ -201,16 +201,16 @@ class RecieveUsersProfile(Resource):
         # So user find is our user object therefore I can subscript user find with the profile picture
 
         if 'profile_picture' in user_find and 'bio' in user_find:
-            homie_collection.replace_one({'profile_picture': requested_json['profile_picture']})
-            homie_collection.replace_one({'bio': requested_json['bio']})
+            homie_collection.updateOne({'profile_picture': requested_json['profile_picture']})
+            homie_collection.updateOne({'bio': requested_json['bio']})
             print('The users profile picture and bio have been replaced')
 
         elif 'profile_picture' in user_find and 'bio' not in user_find:
-            homie_collection.replace_one({'profile_picture': requested_json['profile_picture']})
+            homie_collection.updateOne({'profile_picture': requested_json['profile_picture']})
             print('The users profile pitcure has been replaced')
     
         elif 'bio' in user_find and 'profile_picture' not in user_find:
-            homie_collection.replace_one({'bio': requested_json['bio']})
+            homie_collection.updateOne({'bio': requested_json['bio']})
             print('The users bio has been replaced')
 
         elif 'profile_picture' not in user_find and 'bio' not in user_find:
